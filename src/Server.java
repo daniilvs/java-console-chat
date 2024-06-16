a200d38965dc696a62e05736b6e89146434ba505
dnlvstn <dnlvstn@gmail.com>
2024-06-16 15:55:22 +0300

    working

---
+++ src/Server.java
@@ -0,0 +1,126 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class Server implements Runnable{
+
+    private ServerSocket server;
+    private boolean done;
+    private ArrayList<ConnectionHandler> connections;
+    private ExecutorService pool;
+
+    public Server() {
+        connections = new ArrayList<>();
+        done = false;
+    }
+
+    @Override
+    public void run() {
+        try {
+            server = new ServerSocket(9999);
+            pool = Executors.newCachedThreadPool();
+            while (!done) {
+                Socket client = server.accept();
+                ConnectionHandler handler = new ConnectionHandler(client);
+                connections.add(handler);
+                pool.execute(handler);
+            }
+        } catch (Exception e) {
+            shutdown();
+        }
+    }
+
+    public void broadcast(String message) {
+        for (ConnectionHandler ch : connections) {
+            if (ch != null) {
+                ch.sendMessage(message);
+            }
+        }
+    }
+
+    public void shutdown() {
+        try {
+            done = true;
+            pool.shutdown();
+            if (!server.isClosed()) {
+                server.close();
+            }
+            for (ConnectionHandler ch: connections) {
+                ch.shutdown();
+            }
+        } catch (IOException e) {
+            //idnore
+        }
+    }
+
+    class ConnectionHandler implements Runnable {
+
+        private Socket client;
+        private BufferedReader in;
+        private PrintWriter out;
+        private String nickname;
+
+        public ConnectionHandler(Socket client) {
+            this.client = client;
+        }
+
+        @Override
+        public void run() {
+            try {
+                out = new PrintWriter(client.getOutputStream(), true);
+                in = new BufferedReader(new InputStreamReader(client.getInputStream()));
+                out.println("Введите имя: ");
+                nickname = in.readLine();
+                System.out.println(nickname + " присоединился");
+                broadcast(nickname + " вошел в чат");
+                String message;
+                while ((message = in.readLine()) != null) {
+                    if (message.startsWith("/nick ")) {
+                        String[] messageSplit = message.split(" ", 2);
+                        if (messageSplit.length == 2) {
+                            broadcast(nickname + " поменял никнейм на " + messageSplit[1]);
+                            nickname = messageSplit[1];
+                            out.println("Ник успешно изменен на " + nickname);
+                        } else {
+                            out.println("Не был введен новый ник ");
+                        }
+                    } else if (message.startsWith("/quit")) {
+                        broadcast(nickname + " покинул нас");
+                        shutdown();
+                    } else {
+                        broadcast(nickname + ": " + message);
+                    }
+                }
+            } catch (IOException e) {
+                shutdown();
+            }
+        }
+
+        public void sendMessage(String message) {
+            out.println(message);
+        }
+
+        public void shutdown() {
+            try {
+                in.close();
+                out.close();
+                if (!client.isClosed()) {
+                    client.close();
+                }
+            } catch (IOException e) {
+
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Server server = new Server();
+        server.run();
+    }
+}
